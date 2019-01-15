4ef71f484108a231b2b1fada38b5d4d3b6ea8a17
Leather Dog Muksihs <muksihs@gmail.com>
2019-01-15 18:52:07 -0500

    Fix for malformed dates coming back from new hivemind API
    https://github.com/steemit/hivemind/issues/169

--- publish.sh
+++ publish.sh
@@ -2,5 +2,7 @@
 
 cd "$(dirname "$0")"
 
-gradle clean build fatjar -xtest && scp build/libs/Far-Horizons-Steem.jar muksihs@muksihs.com:Far-Horizons/. && date
+./gradlew clean build fatjar -xtest
+rsync --verbose --progress -z build/libs/Far-Horizons-Steem.jar muksihs@muksihs.com:Far-Horizons/.
+date
 
